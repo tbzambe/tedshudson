e75484a0ecab9d08de2f55e068ef6bef48e5126c
Thais Zambe <tbzambe@gmail.com>
2017-08-17 20:36:38 -0300

    Merge of "develop" and "origin/develop"

--- poupancaEmBanco/src/View/View.java
+++ poupancaEmBanco/src/View/View.java
@@ -74,7 +74,57 @@ public class View {
         System.out.println("Taxa administrativa cobrada: " + taxa);
     }
 
-    public static void valorJuros() {
-        System.out.println("Que taxa de juros deve ser aplicada?");
-    }    
+  public static void printMenu() {
+    System.out.println("Menu \n");
+    System.out.println("1 - CARREGAR POUPANÇA \n");
+    System.out.println("2 - Operações\n");
+    System.out.println("3 - SALVAR POUPANÇA \n");
+    System.out.println("9 - Sair \n");
+  }
+
+  public static void printMenuOperacoes() {
+    System.out.println("\n Operações");
+    System.out.println("4 - Depósito");
+    System.out.println("5 - Saque");
+    System.out.println("6 - Aplicar Juros");
+    System.out.println("7 - Exibir status da conta");
+    System.out.println("8 - Voltar");
+
+  }
+
+  public static void getOpcao() {
+    System.out.println("Escolha uma opção: ");
+  }
+
+  public static void exibeSaldo(Double saldo) {
+    System.out.println("Saldo atual: " + saldo);
+  }
+
+  public static void poupancaCarregada() {
+    System.out.println("Poupança carregada!");
+  }
+
+  public static void poupancaSalva() {
+    System.out.println("Poupança salva!");
+  }
+
+  public static void valorDeposito() {
+    System.out.println("Quanto deseja depositar?");
+  }
+
+  public static void valorDepositado(Double valor) {
+    System.out.println("Valor depositado: " + valor);
+  }
+
+  public static void valorSaque() {
+    System.out.println("Quanto deseja sacar?");
+  }
+
+  public static void valorTaxaAdministrativa(Double taxa) {
+    System.out.println("Taxa administrativa cobrada: " + taxa);
+  }
+
+  public static void valorJuros() {
+    System.out.println("Que taxa de juros deve ser aplicada?");
+  }
 }
--- poupancaEmBanco/src/poupancaembanco/Model/PoupancaEmBanco.java
+++ poupancaEmBanco/src/poupancaembanco/Model/PoupancaEmBanco.java
@@ -7,143 +7,135 @@ package poupancaembanco.Model;
 
 import java.io.Serializable;
 
+public class PoupancaEmBanco implements Serializable {
 
+  private static final long serialVersionUID = 5110549514946052395L;
 
-public class PoupancaEmBanco  implements Serializable {
+  private double jurosMensal;
+  private double saldo;
+  private double taxaAdm;
+  private int agencia;
+  private int numeroPoupanca;
+  private String banco;
+  private String nomeCliente;
 
- 
+  //METODO CONSTRUTOR
+  public PoupancaEmBanco(double jurosMensal, int agencia, int numeroPoupanca, double saldo, String nomeCliente,
+      double taxaAdm, String banco) {
+    this.setJurosMensal(jurosMensal);
+    this.setAgencia(agencia);
+    this.setNumeroPoupanca(numeroPoupanca);
+    this.setSaldo(saldo);
+    this.setNomeCliente(nomeCliente);
+    this.setTaxaAdm(taxaAdm);
+    this.setBanco(banco);
+  }
 
-       private static final long serialVersionUID = 5110549514946052395L;
-       
-	private double jurosMensal;
-	private int agencia;
-	private int numeroPoupanca;
-	private double saldo;
-	private String nomeCliente;
-	private double taxaAdm;
-	private String banco;
-	
-	//METODO CONSTRUTOR
-	public PoupancaEmBanco(double jurosMensal, int agencia, int numeroPoupanca, double saldo, String nomeCliente, double taxaAdm, String banco ) {
-		this.setJurosMensal(jurosMensal);
-		this.setAgencia(agencia);
-		this.setNumeroPoupanca(numeroPoupanca);
-		this.setSaldo(saldo);
-		this.setNomeCliente(nomeCliente);
-		this.setTaxaAdm(taxaAdm);
-		this.setBanco(banco);
-		
-	}
-        public PoupancaEmBanco(){
-            this.setJurosMensal(0.0);
-            this.setAgencia(104);
-            this.setNumeroPoupanca(1234);
-            this.setSaldo(0.0);
-            this.setNomeCliente("");
-            this.setTaxaAdm(0.0);
-            this.setBanco("");
-        }
-        /*
-        public void montaPoupanca(){
-            this.setJurosMensal(0.0);
-            this.setAgencia(104);
-            this.setNumeroPoupanca(1234);
-            this.setSaldo(0.0);
-            this.setNomeCliente("");
-            this.setTaxaAdm(0.0);
-            this.setBanco("");
-        }
-        */
-        public void exibeTudo()
-        {
-                System.out.println("\n Taxa de juros mensal: " + this.getJurosMensal());
-		System.out.println("\n Agencia: " + this.getAgencia());
-		System.out.println("\n Num. poupança: " + this.getNumeroPoupanca());
-		System.out.println("\n Saldo: " + this.getSaldo());
-		System.out.println("\n Nome do cliente: " + this.getNomeCliente());
-		System.out.println("\n Taxa adm. :" + this.getTaxaAdm());
-		System.out.println("\n Banco: " + this.getBanco());
-        }
+  public void exibeTudo() {
+    System.out.println("\n Taxa de juros mensal: " + this.getJurosMensal());
+    System.out.println("\n Agencia: " + this.getAgencia());
+    System.out.println("\n Num. poupança: " + this.getNumeroPoupanca());
+    System.out.println("\n Saldo: " + this.getSaldo());
+    System.out.println("\n Nome do cliente: " + this.getNomeCliente());
+    System.out.println("\n Taxa adm. :" + this.getTaxaAdm());
+    System.out.println("\n Banco: " + this.getBanco());
+  }
 
-    
-	
-	public double getJurosMensal() {
-		return jurosMensal;
-	}
-	public void setJurosMensal(double jurosMensal) {
-		this.jurosMensal = jurosMensal;
-	}
-	public int getAgencia() {
-		return agencia;
-	}
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
-	public int getNumeroPoupanca() {
-		return numeroPoupanca;
-	}
-	public void setNumeroPoupanca(int numeroPoupanca) {
-		this.numeroPoupanca = numeroPoupanca;
-	}
-	public double getSaldo() {
-		return saldo;
-	}
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	public String getNomeCliente() {
-		return nomeCliente;
-	}
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
-	}
-	public double getTaxaAdm() {
-		return taxaAdm;
-	}
-	public void setTaxaAdm(double taxaAdm) {
-		this.taxaAdm = taxaAdm;
-	}
-	public String getBanco() {
-		return banco;
-	}
-	public void setBanco(String banco) {
-		this.banco = banco;
-	}
-	
-	//ATUALIZA SALDO COM BASE NOS JUROS MENSAIS
-	public void atualizaSaldo() {
-		this.setSaldo((this.getSaldo()*this.getJurosMensal()));
-	}
-	//ATUALIZA SALDO COM BASE EM UMA TAXA DE JUROS
-	public void atualizaSaldo(Double taxaJuros) {
-		this.setSaldo((this.getSaldo()*taxaJuros));
-	}
-	//ATUALIZA SALDO COM BASE EM UMA TAXA DE JUROS, POREM VALÍDA O NUMERO DA POUPANÇA ANTES
-	public void atualizaSaldo(int numeroPoupanca, Double taxaJuros) {
-        if ((this.getNumeroPoupanca() == numeroPoupanca)) {
-            this.setSaldo(this.getSaldo() * taxaJuros);
-        }
+  public double getJurosMensal() {
+    return jurosMensal;
+  }
+
+  public void setJurosMensal(double jurosMensal) {
+    this.jurosMensal = jurosMensal;
+  }
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public int getNumeroPoupanca() {
+    return numeroPoupanca;
+  }
+
+  public void setNumeroPoupanca(int numeroPoupanca) {
+    this.numeroPoupanca = numeroPoupanca;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+  public String getNomeCliente() {
+    return nomeCliente;
+  }
+
+  public void setNomeCliente(String nomeCliente) {
+    this.nomeCliente = nomeCliente;
+  }
+
+  public double getTaxaAdm() {
+    return taxaAdm;
+  }
+
+  public void setTaxaAdm(double taxaAdm) {
+    this.taxaAdm = taxaAdm;
+  }
+
+  public String getBanco() {
+    return banco;
+  }
+
+  public void setBanco(String banco) {
+    this.banco = banco;
+  }
+
+  /**
+   * Atualiza saldo com base nos juros mensais.
+   */
+  public void atualizaSaldo() {
+    this.setSaldo((this.getSaldo() * this.getJurosMensal()));
+  }
+
+  /**
+   * Atualiza saldo com base em uma taxa de juros.
+   * @param taxaJuros
+   */
+  public void atualizaSaldo(Double taxaJuros) {
+    this.setSaldo((this.getSaldo() * taxaJuros));
+  }
+
+  /**
+   * Atualiza saldo com base em uma taxa de juros, porem valida o numero da poupanca antes.
+   * @param numeroPoupanca
+   * @param taxaJuros
+   */
+  public void atualizaSaldo(int numeroPoupanca, Double taxaJuros) {
+    if ((this.getNumeroPoupanca() == numeroPoupanca)) {
+      this.setSaldo(this.getSaldo() * taxaJuros);
     }
-	//OPERAÇÕES
-	public void operacao(int operacao, Double valor) {
-		switch(operacao) {
-        case '1':
-            this.setSaldo(this.getSaldo() + valor);
-        break;
-        case '2':
-        	this.setSaldo(this.getSaldo() - valor);
-        	this.setSaldo(this.getSaldo() - this.getTaxaAdm());           
-        break;
-        case '3':
-        	this.setSaldo(this.getSaldo() * (1+(valor/100)));
-        break;
-		}
-	}
-	
-	
-	
-	
-	
-	
-    
+  }
+
+  //OPERAÇÕES
+  public void operacao(int operacao, Double valor) {
+    switch (operacao) {
+    case '1':
+      this.setSaldo(this.getSaldo() + valor);
+      break;
+    case '2':
+      this.setSaldo(this.getSaldo() - valor);
+      this.setSaldo(this.getSaldo() - this.getTaxaAdm());
+      break;
+    case '3':
+      this.setSaldo(this.getSaldo() * (1 + (valor / 100)));
+      break;
+    }
+  }
 }
